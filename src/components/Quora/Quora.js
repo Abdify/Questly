8d1ebebf8442cf00d61067dd232920db16fd61b1
Nabil Woalid Rafiu <nabilwoalidrafiu@gmail.com>
2021-05-28 21:55:08 +0600

    post ui

--- src/components/Quora/Quora.js
+++ src/components/Quora/Quora.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Feed from '../Feed/Feed';
+import Post from '../Post/Post';
 
 const Quora = () => {
     return (
         <div>
-            Hello
+            <Feed></Feed>
         </div>
     );
 };
