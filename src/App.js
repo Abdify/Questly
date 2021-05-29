e4a1f4b71fbf59b97261fe471fd63e9677577dac
Abdify <abdullah.fahimm08@gmail.com>
2021-05-29 21:56:06 +0600

    user login in app added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
-import Quora from './components/Quora/Quora';
+import { useSelector } from "react-redux";
+import "./App.css";
+import CheckUser from "./components/Auth/CheckUser";
+import Login from "./components/Auth/Login/Login";
+import Quora from "./components/Quora/Quora";
+import { selectUser } from "./Redux/userSlice";
 function App() {
-  return (
-    <div className="app">
-      <Quora />
-    </div>
-  );
+    const user = useSelector(selectUser);
+    CheckUser();
+
+    return <div>{user ? <Quora /> : <Login />}</div>;
 }
 
 export default App;
