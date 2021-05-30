f7c13eb0ac338cd9c42f4eddba4c74e8c9e6033f
JisanFahim <JisanFahim@users.noreply.github.com>
2021-05-30 20:33:37 +0600

    modal style added

--- src/components/SidebarOption/SidebarOption.js
+++ src/components/SidebarOption/SidebarOption.js
@@ -1,49 +1,38 @@
 import { Add } from '@material-ui/icons';
 import React from 'react';
-import './SidebarOption.css'
+import './SidebarOption.css';
+import logo from '../../images/logo.png';
 
 const SidebarOption = () => {
     return (
         <div className="sidebarOptions">
             <div className="sidebarOption">
-                <img
-                    src="https://lh3.googleusercontent.com/proxy/L8_cQpEf5-ChYhM3IPQpyjGqUTbrK3bTQlgN1QswgRyet4JjwlK3fDPnmehz99ZGd6N4dV_hHCe7mbXE7A1uywKRyxBzHLiSuDn3muGnicbz"
-                    alt="sidebar-img" />
+                <img src={logo} alt="sidebar-img" />
                 <p>History</p>
             </div>
             <div className="sidebarOption">
-                <img
-                    src="https://lh3.googleusercontent.com/proxy/L8_cQpEf5-ChYhM3IPQpyjGqUTbrK3bTQlgN1QswgRyet4JjwlK3fDPnmehz99ZGd6N4dV_hHCe7mbXE7A1uywKRyxBzHLiSuDn3muGnicbz"
-                    alt="sidebar-img" />
+                <img src={logo} alt="sidebar-img" />
                 <p>History</p>
             </div>
             <div className="sidebarOption">
-                <img
-                    src="https://lh3.googleusercontent.com/proxy/L8_cQpEf5-ChYhM3IPQpyjGqUTbrK3bTQlgN1QswgRyet4JjwlK3fDPnmehz99ZGd6N4dV_hHCe7mbXE7A1uywKRyxBzHLiSuDn3muGnicbz"
-                    alt="sidebar-img" />
+                <img src={logo} alt="sidebar-img" />
                 <p>History</p>
             </div>
             <div className="sidebarOption">
-                <img
-                    src="https://lh3.googleusercontent.com/proxy/L8_cQpEf5-ChYhM3IPQpyjGqUTbrK3bTQlgN1QswgRyet4JjwlK3fDPnmehz99ZGd6N4dV_hHCe7mbXE7A1uywKRyxBzHLiSuDn3muGnicbz"
-                    alt="sidebar-img" />
+                <img src={logo} alt="sidebar-img" />
                 <p>History</p>
             </div>
             <div className="sidebarOption">
-                <img
-                    src="https://lh3.googleusercontent.com/proxy/L8_cQpEf5-ChYhM3IPQpyjGqUTbrK3bTQlgN1QswgRyet4JjwlK3fDPnmehz99ZGd6N4dV_hHCe7mbXE7A1uywKRyxBzHLiSuDn3muGnicbz"
-                    alt="sidebar-img" />
+                <img src={logo} alt="sidebar-img" />
                 <p>History</p>
             </div>
             <div className="sidebarOption">
-                <img
-                    src="https://lh3.googleusercontent.com/proxy/L8_cQpEf5-ChYhM3IPQpyjGqUTbrK3bTQlgN1QswgRyet4JjwlK3fDPnmehz99ZGd6N4dV_hHCe7mbXE7A1uywKRyxBzHLiSuDn3muGnicbz"
-                    alt="sidebar-img" />
+                <img src={logo} alt="sidebar-img" />
                 <p>History</p>
             </div>
             <div className="sidebarOption">
                 <Add />
-                <p>HHHHKK</p>
+                <p>Add More</p>
             </div>
         </div>
     );
--- src/components/WidgetContent/WidgetContent.js
+++ src/components/WidgetContent/WidgetContent.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import './WidgetContent.css';
+import logo from '../../images/logo.png';
 
 const WidgetContent = () => {
     return (
         <div className="widget_contents">
             <div className="widget_content">
-                <img
-                    src="https://lh3.googleusercontent.com/proxy/L8_cQpEf5-ChYhM3IPQpyjGqUTbrK3bTQlgN1QswgRyet4JjwlK3fDPnmehz99ZGd6N4dV_hHCe7mbXE7A1uywKRyxBzHLiSuDn3muGnicbz"
-                    alt="" />
+                <img src={logo} alt="widget-img" />
                 <div className="widget_content_title">
                     <h5>Mobile App Programmer</h5>
                     <p>The Mobile App Development Company.</p>
                 </div>
             </div>
             <div className="widget_content">
-                <img
-                    src="https://lh3.googleusercontent.com/proxy/L8_cQpEf5-ChYhM3IPQpyjGqUTbrK3bTQlgN1QswgRyet4JjwlK3fDPnmehz99ZGd6N4dV_hHCe7mbXE7A1uywKRyxBzHLiSuDn3muGnicbz"
-                    alt="" />
+                <img src={logo} alt="widget-img" />
                 <div className="widget_content_title">
                     <h5>Mobile App Programmer</h5>
                     <p>The Mobile App Development Company.</p>
                 </div>
             </div>
             <div className="widget_content">
-                <img
-                    src="https://lh3.googleusercontent.com/proxy/L8_cQpEf5-ChYhM3IPQpyjGqUTbrK3bTQlgN1QswgRyet4JjwlK3fDPnmehz99ZGd6N4dV_hHCe7mbXE7A1uywKRyxBzHLiSuDn3muGnicbz"
-                    alt="" />
+                <img src={logo} alt="widget-img" />
                 <div className="widget_content_title">
                     <h5>Mobile App Programmer</h5>
                     <p>The Mobile App Development Company.</p>
